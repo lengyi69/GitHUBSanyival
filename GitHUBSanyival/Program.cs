ad4758483d5758a352c1a5cec86cccbc437dbe64
LI <LI@DESKTOP-VDOGF3H>
2021-09-02 14:42:47 +0200

    Add project files.

---
+++ GitHUBSanyival/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHUBSanyival
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Második terítés!");
+        }
+    }
+}
